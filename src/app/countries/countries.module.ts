bd762f92c06f0062dea6ef423db0eea00243ee65
RapNautico <foox299@gmail.com>
2022-12-29 16:19:28 -0500

    create folders for countries and subscribers

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CountriesRoutingModule } from './countries-routing.module';
+import { CountriesComponent } from './countries.component';
+import { ListCountriesComponent } from './list-countries/list-countries.component';
+
+
+@NgModule({
+  declarations: [
+    CountriesComponent,
+    ListCountriesComponent
+  ],
+  imports: [
+    CommonModule,
+    CountriesRoutingModule
+  ]
+})
+export class CountriesModule { }
---
+++ src/app/subscribers/show-subscribers/show-subscribers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-subscribers',
+  templateUrl: './show-subscribers.component.html',
+  styleUrls: ['./show-subscribers.component.css']
+})
+export class ShowSubscribersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
